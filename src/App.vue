27bde507f7fe82d84cdb743eea717f5ba065f765
mertsincan <sincan.mert@gmail.com>
2022-04-12 13:57:07 +0100

    Update demo

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <span class="logo-wrapper">
+    <img alt="Vue logo" src="./assets/vite-logo.svg" />
+    <i class="pi pi-plus" style="font-size: 2rem" />
+    <img alt="PrimeVue logo" src="./assets/primevue-logo.png" />
+  </span>
+  
+  <Home msg="Welcome to PrimeVue!" />
 </template>
 
 <style>
@@ -17,5 +22,14 @@ import HelloWorld from './components/HelloWorld.vue'
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.logo-wrapper {
+  display: inline-flex;
+  align-items: center;
+}
+
+.logo-wrapper img {
+  width: 150px;
 }
 </style>
